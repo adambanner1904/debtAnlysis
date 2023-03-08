cd7bf81ef46c56a5b22c006d82ef67bdf8defa32
adambanner1904 <120569454+adambanner1904@users.noreply.github.com>
2023-03-08 17:36:31 +0000

    Continued adding the functionality. Starting to make it possible to pass years and months of time.

--- simple_debt.py
+++ simple_debt.py
@@ -8,15 +8,20 @@ class Debt:
         self.salary = salary
         self.pbr = payback_rate
 
+        # Used to keep track of how long it takes to pay a debt
+        self.years = 0
+        self.months = 0
+
+
     def accumulate_interest(self):
 
         self.amount += self.amount * self.ir
 
     def make_payment(self):
-
+        """Making a monthly payment. """
         # If the amount is positive, if there is still a debt, then continue to making a payment
-        if self.amount > 0 or not self.amount - self.salary * self.pbr:
-            self.amount -= self.salary * self.pbr
+        if self.amount > 0 or not self.amount - self.salary * self.pbr/12:
+            self.amount -= self.salary * self.pbr/12
         else:
             print("There is no more debt")
 
@@ -30,99 +35,23 @@ class Debt:
             return 1
 
     def pass_year(self):
-        """There are three options for both the interest accumulation interval and
-        the payment transaction interval. The outer if statements are where the interest interval is daily
-        monthly or yearly as these are the most common. The inner if statements all cycle through where the
-        payments are made daily, weekly or monthly working on the assumption that debts are not really
-        paid yearly. The default settings for the Debt class are making payments monthly and interest yearly."""
-        self.days = 0
+        """The interest is added at the end of the year"""
 
-        # If the interest is daily
-        if self.interest_interval.lower() == 'd':
+        # Make payment every month of the year
+        for m in range(12):
 
-            # If the payments are made daily
-            if self.pay_interval.lower() == 'd':
+            if self.check_amount():
+                self.make_payment()
+                self.months += 1
+            else:
+                break
 
-                for d in range(365):
+        # Accumulate interest once a year
+        self.accumulate_interest()
 
-                    self.accumulate_interest()
-                    self.make_payment()
-
-
-            # If payments are made weekly
-            if self.pay_interval.lower() == 'w':
-
-                # Make payment once a week
-                for w in range(52):
-                    self.make_payment()
-
-                    # Accumulate interest every day of every week
-                    for d in range(7):
-                        self.accumulate_interest()
-
-            # If payments are made monthly
-            if self.pay_interval.lower() == 'm':
-
-                # Make payment once a month
-                for m in range(12):
-                    self.make_payment()
-
-                    # Accumulate interest every day every month
-                    for d in range(30):
-                        self.accumulate_interest()
-        ########################
-        # If the interest is monthly
-        if self.interest_interval.lower() == 'm':
-
-            # If payments are made daily
-            if self.pay_interval.lower() == 'd':
-
-                # Accumulate interest once a month
-                for m in range(12):
-                    self.accumulate_interest()
-
-                    # Make payment every day every month
-                    for d in range(30):
-                        self.make_payment()
-
-            # If payments are made weekly
-            if self.pay_interval.lower() == 'w':
-
-                # Accumulate interest once a month
-                for m in range(12):
-                    self.accumulate_interest()
-
-                    # Make payment every week of every month
-                    for w in range(4):
-                        self.make_payment()
-
-
-            # If payments are made monthly
-            if self.pay_interval.lower() == 'm':
-
-                # Make payment once a month
-                for m in range(12):
-                    self.make_payment()
-                    self.accumulate_interest()
-
-        ########################
-        # If the interest is yearly
-        if self.interest_interval.lower() == 'y':
-
-            # If payments are made monthly
-            if self.pay_interval.lower() == 'm':
-
-                # Accumulate interest once a year
-                self.accumulate_interest()
-
-                # Make payment every month of the year
-                for m in range(12):
-
-                    if self.check_amount():
-                        self.make_payment()
-                    else:
-                        break
-                    self.days += 30
+    def pass_month(self):
+        pass
+        # Make payment
 
 
 
@@ -130,6 +59,6 @@ debt = Debt(40, 40, 0.1, 0.06)
 
 print(debt.amount)
 debt.pass_year()
-print(debt.days)
+print(debt.months)
 
 print(debt.amount)
