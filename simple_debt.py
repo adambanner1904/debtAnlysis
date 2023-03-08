8a9c7c3d3ad56ce540d638f4f91821824cc78d27
adambanner1904 <120569454+adambanner1904@users.noreply.github.com>
2023-03-08 18:16:09 +0000

    Added several tracking attributes. Added income from salary by subtracting the tax.

--- simple_debt.py
+++ simple_debt.py
@@ -5,23 +5,51 @@ class Debt:
 
         self.amount = amount
         self.ir = interest_rate
-        self.salary = salary
         self.pbr = payback_rate
+
+        # Calculating income after tax
+        self.salary = salary
+
+        if self.salary <= 12.570:
+            self.income = self.salary
+        elif 12.570 < self.salary <= 50.270:
+            pa = 12.570
+            br = self.salary - pa
+            self.income = pa + br * 0.8
+        elif 50.270 < self.salary <= 150.0000:
+            pa = 12.570
+            br = 50.270 - 12.570
+            hr = self.salary - br - pa
+            self.income = pa + br * 0.8 + hr * 0.6
+        elif 150.000 < self.salary:
+            pa = 12.570
+            br = 50.270 - 12.570
+            hr = 150.000
+            ar = self.salary - hr - br - pa
+            self.income = pa + br * 0.8 + hr * 0.6 + ar * 0.55
+
+        # Calculating monthly payments and tracking payments
+        self.payment = self.income * self.pbr/12
+        self.total_payment = 0
+
+        # Used to track the interest
+        self.interest = 0
 
         # Used to keep track of how long it takes to pay a debt
         self.years = 0
         self.months = 0
 
-
     def accumulate_interest(self):
+        """Interest is yearly"""
 
         self.amount += self.amount * self.ir
+        self.interest += self.amount * self.ir
 
     def make_payment(self):
-        """Making a monthly payment. """
+        """Making a monthly payment."""
         # If the amount is positive, if there is still a debt, then continue to making a payment
-        if self.amount > 0 or not self.amount - self.salary * self.pbr/12:
-            self.amount -= self.salary * self.pbr/12
+        if self.check_amount():
+            self.amount -= self.payment
         else:
             print("There is no more debt")
 
@@ -29,7 +57,7 @@ class Debt:
         """Checks to see if the amount of the debt is positive and if the next payment
         will make the debt negative. Returns true (1) if there is still an amount and
         false (0) if the debt is done."""
-        if self.amount <= 0 or self.amount - self.salary * self.pbr < 0:
+        if self.amount <= 0 or self.amount - self.income * self.pbr/12 < 0:
             return 0
         else:
             return 1
@@ -44,8 +72,9 @@ class Debt:
                 self.make_payment()
                 self.months += 1
             else:
-                break
-
+                return
+        self.years += 1
+        self.months = 0
         # Accumulate interest once a year
         self.accumulate_interest()
 
@@ -54,11 +83,15 @@ class Debt:
         # Make payment
 
 
+debt = Debt(50, 25, 0.09, 0.06)
 
-debt = Debt(40, 40, 0.1, 0.06)
+print(f'Starting debt is: {debt.amount}')
+print(f'Salary is {debt.salary}, Income is {debt.income}')
+while debt.check_amount():
+    debt.pass_year()
+    if debt.years == 31:
+        break
+    print(f'Year {debt.years}: {debt.amount}')
+print(f'It took {debt.years} years and {debt.months} months')
+print(f'Total interest: {debt.interest}')
 
-print(debt.amount)
-debt.pass_year()
-print(debt.months)
-
-print(debt.amount)
